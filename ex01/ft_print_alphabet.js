8a8096b97a15d45d8f535c55a443e3a15f3defe8
etiennelavigne <98097759+etiennelavigne@users.noreply.github.com>
2023-03-12 18:02:07 +0100

    Exo 1 aplphabet array

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,9 @@
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function printAlphabet(alphabetString) {
+  let alphabetcanva = alphabet.split('');
+
+  console.table(alphabetcanva);
+}
+
+printAlphabet(alphabet);
